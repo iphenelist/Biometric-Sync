d5f8810c70d8b5228b7ead940bda11f5eb756e45
oriondigitaltz <oriondigitaltz@yahoo.com>
2026-06-04 12:48:23 +0300

    feat: Update Biometric Device and Shift Type DocTypes with new fields and improved structure

---
+++ biometric_sync/biometric_sync/doctype/shift_type/test_shift_type.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Innocent P Metumba and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestShiftType(FrappeTestCase):
+	pass
